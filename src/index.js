71fd68d2c0547e0c45fa673e020e61c9edb0375d
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-04-17 17:16:11 +0200

    added some js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event){
-    event.preventDefault();
-       
-
+function displayPoem(response){
+    console.log("poem generated");
     new Typewriter("#poem", {
-        strings: "Poem",
+        strings: response.data.answer,
         autoStart: true,
         cursor: "",
       });
+}
+
+
+function generatePoem(event){
+    event.preventDefault();
+ let promptInput= document.querySelector("#user-prompt");  
+let apiKey="dof43157bf3aftdf9163bbd7407de4ba";
+let context="You are a romantic poem expert and love to write poem. Your mission is to generate a poem in basic HTML.Make sure to follow the user instructions below";
+let prompt=`Generate an english poem above ${promptInput.value} `;
+let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
+ 
+console.log("Generating poem");
+console.log(`Prompt:${prompt}`);
+console.log(`Context: ${context}`);
+
+axios.get(apiUrl).then(displayPoem);
 
 
 }
