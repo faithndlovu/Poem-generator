d315a88e22bc31172c97cc93617d7717da6067c8
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-04-17 17:32:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generatePoem(event){
     event.preventDefault();
  let promptInput= document.querySelector("#user-prompt");  
 let apiKey="dof43157bf3aftdf9163bbd7407de4ba";
-let context="You are a romantic poem expert and love to write poem. Your mission is to generate a poem in basic HTML.Make sure to follow the user instructions below";
+let context="You are a romantic poem expert and love to write poem. Your mission is to generate a an english poem.Make sure to follow the user instructions below.Do not include a title";
 let prompt=`Generate an english poem above ${promptInput.value} `;
 let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
  
