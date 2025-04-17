71f0c692aee183bd704b0f18390ca8e6dbe8b671
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-04-17 13:16:08 +0200

    added some js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function generatePoem(event){
+    event.preventDefault();
+       
+
+    new Typewriter("#poem", {
+        strings: "Poem",
+        autoStart: true,
+        cursor: "",
+      });
+
+
+}
+
+
+
+
+let poemformElement= document.querySelector("#poem-generator-form");
+poemformElement=addEventListener("submit", generatePoem),
